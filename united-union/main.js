3c20b427e2aef520c811bf1bfd1ea1238a8046dc
Sean Concannon <sean.concannon@gmail.com>
2020-07-11 06:22:16 -0700

    changes to united-union - also, used git commit --amend to change commit message

--- united-union/main.js
+++ united-union/main.js
@@ -1,15 +1,17 @@
 function uniteUnique(arr,...arrs) {
-  // find the longest array
-  let extent = arr.length;
+ // ...arrs - using the spread operator in case user adds more than one additional array to the arguments
+
+  // find the longest array - completely unecessary for this exercise
+//   let extent = arr.length;
+//   for (let item in arrs){
+//     if (arrs[item].length > extent){
+//       extent = arrs[item].length;
+//     } 
+//   }
   let product = [];
-  for (let item in arrs){
-    if (arrs[item].length > extent){
-      extent = arrs[item].length;
-    } 
-  }
+  //adds the initial array to the subsequent arrays
   arrs.unshift(arr);
-  console.log(arrs);
-   // loop that ends when length of longest array is met
+   // loop through all of the arrays, add any unique value found to result
   for(let item in arrs){
     for(let memb in arrs[item]){
       if(!product.includes(arrs[item][memb])){
